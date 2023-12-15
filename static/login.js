1e70aba25dd5c158bcfe1f940dcabd9661b7825f
Snow <63268442+Theanhdeptrai123@users.noreply.github.com>
2023-12-15 20:12:26 +0800

    .

--- static/login.js
+++ static/login.js
@@ -1,6 +1,3 @@
-// script.js
-
-// JavaScript code for handling the modal
 var forgotPasswordModal = document.getElementById('forgotPasswordModal');
 var forgotPasswordLink = document.getElementById('forgotPasswordLink');
 var closeForgotPasswordModal = document.getElementById('closeForgotPasswordModal');
@@ -19,15 +16,9 @@ window.onclick = function (event) {
     }
 }
 
-// Add logic for handling the "Forgot Password" form submission here
 var submitForgotPassword = document.getElementById('submitForgotPassword');
 var forgotPasswordForm = document.getElementById('forgotPasswordForm');
 
 submitForgotPassword.onclick = function () {
-    // Implement logic for submitting the "Forgot Password" form
-    // You may use AJAX to send the request to the server
-    // Example: You can display a success message or an error message
-    // based on the response from the server
-    // After handling the submission, you may close the modal
     forgotPasswordModal.style.display = 'none';
 }
